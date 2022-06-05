0b3821eff7496867dea1fd3c28c71dd24e580cf7
Wileam Phan <wileam@phan.codes>
2022-06-04 21:23:37 -0400

    Update script

--- build.sh
+++ build.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 
-whatscr() { echo "Wil's GCC easy build script"; }
-whenscr() { echo "Last updated: May 19, 2022 (WYP)"; }
+whatscr() { echo "gccprefab - Wil's GCC easy build script"; }
+whenscr() { echo "Last updated: Jun 4, 2022 (WYP)"; }
 
 usage() { echo "Usage: $0 [version]"; }
 
 stamp() { echo "$(date) $@"; }
 now() { date "+%Y%m%d-%H%M%z"; }
 
+# Bash double substitution
 dblsub() {
-  CAPS="$(echo "${VER}" | tr [:lower:] [:upper:])"
+  CAPS="$(echo $(eval echo \$$2) | tr [:lower:] [:upper:])"
   VAR="$(echo "$1_${CAPS}")"
   export "$1"="$(eval echo "\$${VAR}")"
 }
+
+pusharr() { "$1"+=("$2"); }
+poparr() {}
 
 # Taken from https://stackoverflow.com/a/23342259
 exe() { echo "\$ $@"; "$@"; }
 
 versions() {
   echo "Supported versions:"
-  echo "- 12      Latest GCC 12 release"  
+  echo "- 11      Latest GCC 11 release"
+  echo "- 12      Latest GCC 12 release"
   echo "- master  Latest master branch on the main GCC git repo"
+  echo "- dev     Current development branch"
 }
 
 parsever() {
@@ -36,9 +42,9 @@ parsever() {
     versions
     exit 1
   else
-    dblsub SRCDIR
-    dblsub BLDDIR
-    dblsub TREE
+    dblsub SRCDIR VER
+    dblsub BLDDIR VER
+    dblsub TREE VER
   fi
 }
 
@@ -51,8 +57,96 @@ checkout() {
   fi	
 }
 
+# Read configuration file
+# Usage: `readcfg [error-code] [cfgfile]`
+readcfg() {
+
+  # First, check read access
+  if [ ! -r "$2" ]; then
+    stamp "Cannot read config file $2"
+    exit $1
+  else
+
+    # Internal vars
+    SEC=""
+    FLAG_YES="--enable-"
+    FLAG_NO="--disable-"
+
+    while read -r line; do
+
+      pfx="${line:0:1}"
+
+      if [ "${pfx}" == "[" ]; then
+
+	# Section header
+	SEC="${line:1:(-1)}"
+	verb 1 "Section header: ${SEC}"
+
+      elif [ "${pfx}" == "+" ]; then
+
+	if [ "${SEC}" == "flags" ]; then
+
+	  # Add flag
+	  FLAG="${FLAG_YES}${line:1::}"
+          pusharr FLAGS FLAG
+	  verb 1 "Addition flag: ${FLAG}"
+
+	elif [ "${SEC}" == "languages" ]; then
+
+	  # Add language
+	  LANG="${line}"
+	  pusharr LANGUAGES LANG
+	  verb 1 "Add language: ${LANG}"
+
+	fi # sec
+	    
+      elif [ "${pfx}" == "~" ]; then
+
+	if [ "${SEC}" == "flags" ]; then
+
+	  # Subtract flag
+	  FLAG="${FLAG_NO}${line:1::}"
+          pusharr FLAGS FLAG
+	  verb "Removal flag: ${FLAG}"
+
+	elif [ "${SEC}" == "languages" ]; then
+
+	  # Remove language
+	  LANG="${line}"
+	  poparr LANGUAGES LANG  
+	  verb "Remove language: ${LANG}"
+
+	fi # sec
+
+      else
+
+	if [ "${SEC}" == "version" ]; then
+
+	  # Version code
+	  export VER="${line}"
+	    
+	elif [ "${SEC}" == "flag" ]; then
+
+	  # Normal flag
+	  FLAG="${FLAG_YES}${line:1::}"
+          pusharr FLAGS FLAG
+	  verb 1 "Addition flag: ${FLAG}"
+
+	else
+
+	  # Defaults
+	  def_languages
+
+	fi # sec
+
+      fi # pfx
+
+    done < "$2"
+  fi # readable
+}
+
 # Check status of previous operation
-# Usage: `status [error-code] [phase-name] [logfile]
+# Usage: `status [error-code] [phase-name] [logfile]`
 status() {
   ierr=$?
   errcode=$1  
@@ -72,7 +166,7 @@ status() {
 }
 
 # Threads for make -j
-NTHR=12
+NTHREADS=12
 
 # Git repo URL
 GCCGIT="git://gcc.gnu.org/git/gcc.git"
@@ -80,6 +174,7 @@ GCCGIT="git://gcc.gnu.org/git/gcc.git"
 # Versions
 VER=""
 declare -a VERS
+VERS+=("11")
 VERS+=("12")
 VERS+=("master")
 
@@ -89,10 +184,20 @@ SRCDIR=""
 BLDDIR=""
 TREE=""
 
+# Current development branch
+SRCDIR_DEV="${ROOTDIR}/src"
+BLDDIR_DEV="${ROOTDIR}/build/dev"
+TREE_DEV=$(git branch --show-current)
+
 # GCC master branch
 SRCDIR_MASTER="${ROOTDIR}/src"
 BLDDIR_MASTER="${ROOTDIR}/build/master"
 TREE_MASTER="master"
+
+# GCC 11 release tag
+SRCDIR_11="${ROOTDIR}/src"
+BLDDIR_11="${ROOTDIR}/build/gcc-11"
+TREE_11="releases/gcc-11"
 
 # GCC 12 release tag
 SRCDIR_12="${ROOTDIR}/src"
@@ -106,11 +211,13 @@ PREREQS+=("isl")
 PREREQS+=("mpc")
 PREREQS+=("mpfr")
 
-# Flags
-LANGUAGES="--enable-languages=c,c++,fortran"
-NOBOOTSTRAP="--disable-bootstrap"
-NOSANITIZER="--disable-libsanitizer"
-NOMULTILIB="--disable-multilib"
+# Languages
+declare -a LANGUAGES
+def_languages() {
+  pusharr LANGUAGES "c"
+  pusharr LANGUAGES "c++"
+  pusharr LANGUAGES "fortran"
+}
 
 # Check argc and bail out if no versions are specified
 if [ $# -lt 1 ]; then
@@ -121,16 +228,16 @@ fi
 
 # Set up configure flags
 CFGFLAGS=""
-declare -a CFG
-CFG+=("${LANGUAGES}")
-CFG+=("${NOBOOTSTRAP}")
-CFG+=("${NOSANITIZER}")
-CFG+=("${NOMULTILIB}")
-CFG+=("--prefix=${BLDDIR}")
+declare -a FLAGS
+FLAGS+=("${LANGUAGES}")
+FLAGS+=("${NOBOOTSTRAP}")
+FLAGS+=("${NOSANITIZER}")
+FLAGS+=("${NOMULTILIB}")
+FLAGS+=("--prefix=${BLDDIR}")
 
 # Set up tests
-declare -a TST
-TST+=("check")
+declare -a CHECKS
+CHECKS+=("check-fortran")
 
 # Logfiles
 NOW="$(now)"
@@ -164,9 +271,11 @@ else
 
   pushd "${SRCDIR}"
 
-    # Stash work and checkout selected branch/tag
-    exe git stash push -m "WIP-$(now)"
-    checkout
+    if [ "${VER}" != "dev" ]; then
+      # Stash work and checkout selected branch/tag
+      exe git stash push -m "WIP-$(now)"
+      checkout
+    fi # dev
 
     # Check for prerequisites
     cnt=0
